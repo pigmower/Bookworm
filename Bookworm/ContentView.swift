605a1248b0694ac5d03abd0e0858dcc273ce6307
Tom Arnold <thomas.a.arnold@gmail.com>
2020-07-13 11:12:54 -0700

    Initial Commit

---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Tom on 7/13/20.
+//  Copyright © 2020 retiredGuy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
